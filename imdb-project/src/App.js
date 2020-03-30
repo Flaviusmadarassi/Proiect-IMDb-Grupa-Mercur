bc1c8a2a59b950e599403ca15a295987d0ea577a
AlexandruPosa <alex.posa94@yahoo.com>
2020-03-30 22:41:33 +0300

    structure Browser Router

--- imdb-project/src/App.js
+++ imdb-project/src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "./App.css";
+import { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import  Home  from './pages/HomePage/HomePage';
+import  Search  from './pages/SearchPage/SearchPage';
+import  Details from './pages/MoviesDetailsPage/MovieDetailsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="project-title">This is a project</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>MY IMDB</h2>
+          <nav className="navbar">
+          <div className="navbar-bar-list">
+            <p><Link to={'/'} className="nav-link"> Home </Link></p>
+            <p><Link to={'/search-movie'} className="nav-link">Search Movie</Link></p>
+            <p><Link to={'/movie-details'} className="nav-link">Movie details</Link></p>
+          </div>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/search-movie' component={Search} />
+              <Route path='/movie-details' component={Details} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
