a778d4f3bcc730c3ad1b2b91ab8536e8559b7e4c
andreibojor <bojor.andrei@gmail.com>
2020-04-13 18:18:43 +0300

    carousel-test

--- src/pages/SearchPage/MovieBox.js
+++ src/pages/SearchPage/MovieBox.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import './SearchPage.css';
+import React, { Component } from "react";
+import "./SearchPage.css";
 
 class MovieBox extends Component {
-    render(){
-        return <div className='content-container-movies' key={this.props.movie_details.id}>
-                        <p className = 'movie-title'>{this.props.movie_details.Title}</p>              
-                        <img src={this.props.movie_details.Poster}></img>
-                        <p className = 'movie-genre'>{this.props.movie_details.Genre}</p>
-                        <p className = 'movie-year'>{this.props.movie_details.Year}</p>
-                        <p className = 'raiting-number'><span class="fa fa-star checked"></span>{this.props.movie_details.imdbRating}</p>
-                       
-                               
-        </div>
-    }
+  render() {
+    return (
+      <div
+        className="content-container-movies"
+        key={this.props.movie_details.id}
+      >
+        <p className="movie-title">{this.props.movie_details.Title}</p>
+        <img src={this.props.movie_details.Poster}></img>
+        <p className="movie-genre">{this.props.movie_details.Genre}</p>
+        <p className="movie-year">{this.props.movie_details.Year}</p>
+        <p className="raiting-number">
+          <span class="fa fa-star checked"></span>
+          {this.props.movie_details.imdbRating}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default MovieBox;
--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,75 +1,69 @@
-import React, { Component } from 'react';
-import { fetchMovie } from './FetchMovies';
+import React, { Component } from "react";
+import { fetchMovie } from "./FetchMovies";
 import "./SearchPage.css";
-import  MovieBox from './MovieBox.js'
-
+import MovieBox from "./MovieBox.js";
 
 class Search extends Component {
-
   constructor(props) {
-      super(props);
-      this.state = {
-        inputContent:'',
-        movies: [],
-        isLoaded: false,
-        loading : false,
-        message: ''
-      }
+    super(props);
+    this.state = {
+      inputContent: "",
+      movies: [],
+      isLoaded: false,
+      loading: false,
+      message: "",
+    };
   }
 
-  handleOnInputChange  = ( event ) =>{
+  handleOnInputChange = (event) => {
     const query = event.target.value;
-    this.setState({ inputContent : query , loading: true, message: ''});
-    const all_movies='https://movies-api-siit.herokuapp.com/movies';
-    const searched_movies=all_movies+`?Title=^${query}`; // returns the first 10 movies whose Title contains searched movie
-    
-    fetchMovie(searched_movies).then(json => {
-      console.log('Results after search'+ json);
+    this.setState({ inputContent: query, loading: true, message: "" });
+    const all_movies = "https://movies-api-siit.herokuapp.com/movies";
+    const searched_movies = all_movies + `?Title=^${query}`; // returns the first 10 movies whose Title contains searched movie
 
-      this.setState({ isLoaded: true,
-                      movies: json.results,
-                    })
+    fetchMovie(searched_movies).then((json) => {
+      console.log("Results after search" + json);
+
+      this.setState({ isLoaded: true, movies: json.results });
     });
-   
-  }
+  };
 
   componentDidMount() {
     console.log("mounted");
 
-    fetchMovie().then(json => {
+    fetchMovie().then((json) => {
       console.log(json);
 
-      this.setState({ isLoaded: true,
-                      movies: json.results,
-                    })
+      this.setState({ isLoaded: true, movies: json.results });
     });
   }
- 
+
   render() {
-    const { isLoaded, movies} = this.state;
+    const { isLoaded, movies } = this.state;
     const { inputContent } = this.state;
     //console.warn(this.state);
-    
-  
-    if (!isLoaded){
-      return <div>Loading...</div>
-    }
-    else { 
-      
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
       return (
-          
-          <div className ="search-page-container">
-              <div className="input-container">
-                <input className="search-input"  type ="text" value={inputContent} placeholder="Enter a movie title"  onChange={this.handleOnInputChange}></input>
-                <i className="fa fa-search search-icon" aria-hidden="true"></i>
-              </div>
-              <div className ="all-movies-container">
-                    {
-                    movies.map((movie,index) => 
-                    <MovieBox movie_details = {movie} movie_index={index} />
-                    )}
-              </div>
+        <div className="search-page-container">
+          <div className="input-container">
+            <input
+              className="search-input"
+              type="text"
+              value={inputContent}
+              placeholder="Enter a movie title"
+              onChange={this.handleOnInputChange}
+            ></input>
+            <i className="fa fa-search search-icon" aria-hidden="true"></i>
           </div>
+          <div className="all-movies-container">
+            {movies.map((movie, index) => (
+              <MovieBox movie_details={movie} movie_index={index} />
+            ))}
+          </div>
+        </div>
       );
     }
   }
