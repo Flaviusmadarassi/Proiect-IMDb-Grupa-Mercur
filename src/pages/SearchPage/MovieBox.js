dab8b4f6ec107403629a76f95954fee758759671
AlexandruPosa <alex.posa94@yahoo.com>
2020-04-18 17:18:00 +0300

    advance search functionality

--- src/pages/SearchPage/MovieBox.js
+++ src/pages/SearchPage/MovieBox.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import './SearchPage.css';
 
 class MovieBox extends Component {
-    render(){
-        return <div className='content-container-movies' key={this.props.movie_details.id}>
-                        <p className = 'movie-title'>{this.props.movie_details.Title}</p>              
-                        <img src={this.props.movie_details.Poster}></img>
-                        <p className = 'movie-genre'>{this.props.movie_details.Genre}</p>
-                        <p className = 'movie-year'>{this.props.movie_details.Year}</p>
-                        <p className = 'raiting-number'><span class="fa fa-star checked"></span>{this.props.movie_details.imdbRating}</p>
-                       
-                               
+    render() {
+        return <div className='content-container-movies' >
+            <p className='movie-title'>{this.props.movie_details.Title}</p>
+            <img src={this.props.movie_details.Poster}></img>
+            <p className='movie-genre'>{this.props.movie_details.Genre}</p>
+            <p className='movie-year'>{this.props.movie_details.Year}</p>
+            <p className='raiting-number'><span className="fa fa-star checked"></span>{this.props.movie_details.imdbRating}</p>
+
+
         </div>
     }
 }
