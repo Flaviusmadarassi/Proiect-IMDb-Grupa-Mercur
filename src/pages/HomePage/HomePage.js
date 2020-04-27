de65e08479e728606450257cc3d2fd5d41eca68e
Flaviusmadarassi <flavius.madarassi@yahoo.com>
2020-04-27 17:31:09 +0300

    Register

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import LogIn from "../../components/LogIn/LogIn.js";
 
 class Home extends Component {
   render() {
     return (
-        <div className="contentHome">
-          <h2>Home</h2>
-        </div>
+      <div className="contentHome">
+        <LogIn></LogIn>
+      </div>
     );
   }
 }
