ecd22895270894a3ede3ddf93ffb354defbba587
Flaviusmadarassi <flavius.madarassi@yahoo.com>
2020-05-12 04:09:33 +0300

    Logout functionality

--- src/components/LogIn/LogOut.js
+++ src/components/LogIn/LogOut.js
@@ -1,27 +1,21 @@
 export default function logOutFunctionality() {
   const url = "https://movies-app-siit.herokuapp.com/auth/logout";
-  const cookie = document.cookie;
-  let token;
-  let tokenName;
-
-  if (document.cookie !== "") {
-    tokenName = cookie.split("=")[0];
-  } else console.log("TOKEN DOES NOT EXIST");
-
-  console.log(tokenName);
-
-  if (tokenName === "token") {
-    token = cookie.split("=")[1];
+  if (
+    document.cookie.split(";").some((item) => item.trim().startsWith("token="))
+  ) {
     fetch(url, {
       method: "GET", // *GET, POST, PUT, DELETE, etc.
       headers: {
         "Content-Type": "application/json",
-        "X-Auth-Token": token,
+        "X-Auth-Token": document.cookie.split("=")[1],
       },
     })
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
+        document.cookie =
+          "token=; expires=Thu, 13 Sep 1996 00:00:00 UTC; path=/;";
+        window.location.reload();
       });
   }
 }
