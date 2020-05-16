9c60665fb2b32e704ca7c8eef70b7a44c3a5577a
unknown <cebotari.luminita1@gmail.com>
2020-05-16 20:47:10 +0300

    search icon

--- src/components/Header/HeaderAuth.js
+++ src/components/Header/HeaderAuth.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import Nav from "react-bootstrap/Nav";
 import HeaderSearch from "./HeaderSearch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 class HeaderAuth extends Component {
   state = {};
   render() {
     return (
       <div className="ml-auto">
         <HeaderSearch></HeaderSearch>
+        
         <Nav.Link
           href="#"
           className="d-flex align-items-center justify-content-center rounded sign-in-btn"
         >
           <i class="icon"></i>
           <span>sign in</span>
+          
         </Nav.Link>
+        <div>
+          <FontAwesomeIcon icon={faSearch} size="2x" />
+          </div>
       </div>
     );
   }
